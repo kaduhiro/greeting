11cf03364edc52bc1a922bb205bc1546e8b45b2c
kaduhiro <github@kaduhiro.com>
2023-03-03 08:40:41 +0900

    fix:  add property username to user in session

--- src/components/parts/Auth.tsx
+++ src/components/parts/Auth.tsx
@@ -31,16 +31,16 @@ export const Auth = () => {
       <Link href={`https://twitter.com`} target='_blank'>
         <img
           className='h-10 w-10 rounded-full border-2 border-transparent p-0.5 transition duration-200 hover:border-gray-200 hover:opacity-80'
-          src={session.user.image}
+          src={session.user?.image ?? ''}
           alt='icon'
         />
       </Link>
       <Link
-        href={`https://twitter.com/${session.user.username}`}
+        href={`https://twitter.com/${session.user?.username}`}
         target='_blank'
         className='pr-2 text-gray-500 hover:underline'
       >
-        {session.user.name}
+        {session.user?.name}
       </Link>
       <Button
         className='bg-red-600 hover:bg-red-700 focus:ring-red-500'
