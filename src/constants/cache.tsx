f637371bea66f24f4a909c2feefc4168de89bbfd
kaduhiro <github@kaduhiro.com>
2023-03-04 20:34:55 +0900

    feat: add constants for swr cache key

---
+++ src/constants/cache.tsx
@@ -0,0 +1,12 @@
+export const REQUEST_TYPE = {
+  create: 'create',
+  read: 'read',
+  update: 'update',
+  delete: 'delete',
+} as const;
+
+export const TWITTER = {
+  cacheKey: {
+    tweet: 'tweet',
+  },
+} as const;
