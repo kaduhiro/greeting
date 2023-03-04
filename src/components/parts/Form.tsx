d4af18a3623987f8a7acfdb670c03da1c0480f06
kaduhiro <github@kaduhiro.com>
2023-03-04 20:38:57 +0900

    refactor: replace greeting button to component

--- src/components/parts/Form.tsx
+++ src/components/parts/Form.tsx
@@ -1,10 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
+import { Greeting } from '@/components/elements';
 import { Auth } from '@/components/parts';
+import { TweetCreateQuery } from '@/usecases/twitter';
 
 export const Form = () => {
   const titleRef = useRef<HTMLInputElement>(null);
   const bodyRef = useRef<HTMLTextAreaElement>(null);
+
+  const [tweetQuery, setTweetQuery] = useState<TweetCreateQuery>({ title: '', body: '' });
+
+  const onChangeInput = () => {
+    const query = {
+      title: titleRef.current?.value ?? '',
+      body: bodyRef.current?.value ?? '',
+    };
+
+    setTweetQuery(query);
+  };
 
   return (
     <>
@@ -13,21 +26,23 @@ export const Form = () => {
           <input
             type='text'
             className='w-full rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 dark:text-gray-100 dark:focus:text-gray-700'
+            placeholder='title'
             ref={titleRef}
+            onChange={onChangeInput}
           />
         </div>
         <div className='w-full p-2'>
           <textarea
             className='h-40 w-full resize-none overflow-hidden rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-6 text-gray-900 outline-none transition duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 disabled:opacity-50 dark:text-gray-100 dark:focus:text-gray-700'
             maxLength={140}
+            placeholder='body'
             ref={bodyRef}
+            onChange={onChangeInput}
           ></textarea>
         </div>
         <div className='flex w-full justify-between p-2'>
           <Auth />
-          <button className='flex h-10 items-center rounded border-0 bg-blue-500 py-2 px-8 text-lg capitalize text-white duration-200 hover:bg-blue-600 focus:outline-none'>
-            greeting
-          </button>
+          <Greeting query={tweetQuery} />
         </div>
       </div>
     </>
