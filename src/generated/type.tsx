7366173abf519175c93fbee712dc7f8ad5e77c2e
kaduhiro <github@kaduhiro.com>
2023-03-06 07:44:11 +0900

    feat: add api endpoint assisted by AI

--- src/generated/type.tsx
+++ src/generated/type.tsx
@@ -7,3 +7,11 @@ export type ResponseTweetData = {
   text: string;
   created_at: string;
 };
+
+export type RequestAssistantData = {
+  prompt: string;
+};
+
+export type ResponseAssistantData = {
+  text: string;
+};
