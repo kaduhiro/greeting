630e397d155fee604dc6b4aaed582c19364f9f36
kaduhiro <github@kaduhiro.com>
2023-03-07 07:56:37 +0900

    feat: use assistant component

--- src/components/parts/Form.tsx
+++ src/components/parts/Form.tsx
@@ -1,16 +1,21 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import { Greeting } from '@/components/elements';
+import { useRecoilValue } from 'recoil';
+
+import { Assistant, Greeting } from '@/components/elements';
 import { Auth } from '@/components/parts';
+import { assistantState } from '@/states';
 import { TweetCreateQuery } from '@/usecases/twitter';
 
 export const Form = () => {
   const titleRef = useRef<HTMLInputElement>(null);
   const bodyRef = useRef<HTMLTextAreaElement>(null);
 
+  const assistant = useRecoilValue(assistantState);
+
   const [tweetQuery, setTweetQuery] = useState<TweetCreateQuery>({ title: '', body: '' });
 
-  const onChangeInput = () => {
+  const updatePrompt = () => {
     const query = {
       title: titleRef.current?.value ?? '',
       body: bodyRef.current?.value ?? '',
@@ -18,6 +23,13 @@ export const Form = () => {
 
     setTweetQuery(query);
   };
+
+  useEffect(() => {
+    if (bodyRef.current) {
+      bodyRef.current.value = assistant.data?.message.text ?? '';
+      updatePrompt();
+    }
+  }, [assistant]);
 
   return (
     <>
@@ -28,17 +40,23 @@ export const Form = () => {
             className='w-full rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 dark:text-gray-100 dark:focus:text-gray-700'
             placeholder='title'
             ref={titleRef}
-            onChange={onChangeInput}
+            onChange={updatePrompt}
           />
         </div>
         <div className='w-full p-2'>
-          <textarea
-            className='h-40 w-full resize-none overflow-hidden rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-6 text-gray-900 outline-none transition duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 disabled:opacity-50 dark:text-gray-100 dark:focus:text-gray-700'
-            maxLength={140}
-            placeholder='body'
-            ref={bodyRef}
-            onChange={onChangeInput}
-          ></textarea>
+          <div className='relative'>
+            <textarea
+              className='h-40 w-full resize-none overflow-hidden rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-6 text-gray-900 outline-none transition duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 disabled:opacity-50 dark:text-gray-100 dark:focus:text-gray-700'
+              maxLength={140}
+              placeholder='body'
+              ref={bodyRef}
+              onChange={updatePrompt}
+            ></textarea>
+            <div className='absolute left-0 bottom-4'>
+              <Assistant />
+            </div>
+            <span className='absolute right-4 bottom-4 text-gray-500'>{bodyRef.current?.value.length}</span>
+          </div>
         </div>
         <div className='flex w-full justify-between p-2'>
           <Auth />
