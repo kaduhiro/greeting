8ee2b547b8ba3d89858a73b6711ed673df269326
kaduhiro <github@kaduhiro.com>
2023-03-07 08:03:05 +0900

    feat: add animation of TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       },
       animation: {
         'slide-in-elliptic-top-fwd': 'slide-in-elliptic-top-fwd 0.7s cubic-bezier(0.250, 0.460, 0.450, 0.940)   both',
+        heartbeat: 'heartbeat 1.5s ease  infinite both',
       },
       keyframes: {
         'slide-in-elliptic-top-fwd': {
@@ -20,6 +21,29 @@ module.exports = {
             transform: 'translateY(0) rotateX(0) scale(1)',
             'transform-origin': '50% 1400px',
             opacity: '1',
+          },
+        },
+        heartbeat: {
+          '0%': {
+            transform: 'scale(1)',
+            'transform-origin': 'center center',
+            'animation-timing-function': 'ease-out',
+          },
+          '10%': {
+            transform: 'scale(.91)',
+            'animation-timing-function': 'ease-in',
+          },
+          '17%': {
+            transform: 'scale(.98)',
+            'animation-timing-function': 'ease-out',
+          },
+          '33%': {
+            transform: 'scale(.87)',
+            'animation-timing-function': 'ease-in',
+          },
+          '45%': {
+            transform: 'scale(1)',
+            'animation-timing-function': 'ease-out',
           },
         },
       },
