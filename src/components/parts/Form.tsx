a41f27c8c6613a5e82954353a3bc8f1a6ccb8eb8
kaduhiro <github@kaduhiro.com>
2023-03-09 08:22:43 +0900

    refactor: rename function name

--- src/components/parts/Form.tsx
+++ src/components/parts/Form.tsx
@@ -16,7 +16,7 @@ export const Form = () => {
 
   const [greetingQuery, setGreetingQuery] = useState<TweetCreateQuery>({ title: '', body: '' });
 
-  const updatePrompt = () => {
+  const updateForm = () => {
     const query = {
       title: titleRef.current?.value ?? '',
       body: bodyRef.current?.value ?? '',
@@ -38,7 +38,7 @@ export const Form = () => {
   useEffect(() => {
     if (bodyRef.current) {
       bodyRef.current.value = assistant.data?.message.text ?? '';
-      updatePrompt();
+      updateForm();
     }
   }, [assistant]);
 
@@ -51,7 +51,7 @@ export const Form = () => {
             className='w-full rounded border border-gray-300 bg-gray-100/20 py-3 px-4 text-base leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 dark:text-gray-100 dark:focus:text-gray-700'
             placeholder='title'
             ref={titleRef}
-            onChange={updatePrompt}
+            onChange={updateForm}
           />
         </div>
         <div className='flex w-1/3 items-center justify-center p-2'>
@@ -64,7 +64,7 @@ export const Form = () => {
               maxLength={140}
               placeholder='body'
               ref={bodyRef}
-              onChange={updatePrompt}
+              onChange={updateForm}
             ></textarea>
             <div className='absolute left-0 bottom-4'>
               <Assistant />
