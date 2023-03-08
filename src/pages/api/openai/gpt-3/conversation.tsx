eb8779fd9279a134f0e5fd5c0f51e4ec6fd59b34
kaduhiro <github@kaduhiro.com>
2023-03-08 21:10:13 +0900

    refactor: add parameters for GPT-3

--- src/pages/api/openai/gpt-3/conversation.tsx
+++ src/pages/api/openai/gpt-3/conversation.tsx
@@ -25,6 +25,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<ResponseAssista
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
       prompt: prompt,
+      max_tokens: 1024,
+      temperature: 0.5,
+      top_p: 1,
+      frequency_penalty: 1,
+      presence_penalty: 1,
     });
 
     res.status(StatusCodes.OK).json({ text: completion.data.choices[0].text ?? '' });
